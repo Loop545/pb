e11c95d8bd91ee0d940f195a101b9f3f57277705
Pranab Amatya <pranab.aamtya.pa@gmail.com>
2021-07-26 19:37:41 +1000

    Intro page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 
 //components
 import Blogs from './components/Blog'
+import Comingsoon from './components/Comingsoon';
 import { Component } from 'react';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Blogs />
+       {/* <Blogs /> */}
+       <Comingsoon />
       </div>
     );
   }
---
+++ src/components/Comingsoon.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './Comingsoon.scss'
+
+export class Comingsoon extends Component {
+    render() {
+        return (
+            <div className='intro-title'>
+                <h1>Coming soon....</h1>
+            </div>
+        )
+    }
+}
+
+export default Comingsoon
