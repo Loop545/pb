0893b9028638658ad538b78e25d25d42dc5e90fb
Pranab Amatya <pranab.aamtya.pa@gmail.com>
2021-09-11 13:41:27 +1000

    Config

--- src/components/partical.js
+++ src/components/partical.js
@@ -19,7 +19,7 @@ class Partical extends Component {
 						},
 						line_linked: {
 							enable: true,
-							opacity: 0.02,
+							opacity: 0.2,
 						},
 						move: {
 							direction: 'none',
@@ -42,20 +42,24 @@ class Partical extends Component {
 						zIndex: 1,
 					},
 					interactivity: {
+						detect_on: 'canvas',
 						events: {
-							onclick: {
-								enable: true,
-								mode: 'push',
-							},
-							onHover: {
-								enable: true,
-								mode: 'connect',
-							},
+							onhover: { enable: true, mode: 'repulse' },
+							onclick: { enable: true, mode: 'push' },
+							resize: true,
 						},
 						modes: {
-							push: {
-								particles_nb: 1,
+							grab: { distance: 400, line_linked: { opacity: 1 } },
+							bubble: {
+								distance: 400,
+								size: 40,
+								duration: 2,
+								opacity: 8,
+								speed: 3,
 							},
+							repulse: { distance: 200, duration: 0.4 },
+							push: { particles_nb: 4 },
+							remove: { particles_nb: 2 },
 						},
 					},
 					retina_detect: true,
